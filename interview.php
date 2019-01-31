4a3196247813a760c98eec20153b55f275a3957a
Carl Reed <carlreedw@gmail.com>
2019-01-31 15:05:20 -0600

    added functions and mock version of functions to work with CAT-MH API and also dev/test interface without API credentials

---
+++ interview.php
@@ -0,0 +1,11 @@
+<?php
+// $params = [
+	// "project_id" => $module->fromSurvey->pid,
+	// "return_format" => 'array',
+	// "records" => [$module->fromSurvey->record],
+	// "fields" => ["firstname", "lastname"],
+	// "events" => [$module->fromSurvey->eid]
+// ];
+// $record = \REDCap::getData($params);
+
+exit("<pre>" . var_dump($module->fromSurvey) . "</pre>");
