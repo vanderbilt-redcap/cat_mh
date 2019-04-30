6eddd54f5205baf714d5150696deccce0c89a1e6
Carl Reed <carl.w.reed@vumc.org>
2019-04-30 09:16:51 -0500

    updated cron project to run for all enabled projects

--- CAT_MH.php
+++ CAT_MH.php
@@ -82,70 +82,77 @@ class CAT_MH extends \ExternalModules\AbstractExternalModule {
 	
 	// crons
 	public function cronEmail() {
-		\REDCap::logEvent("Running Email Scheduler", time(), NULL, NULL, NULL, $this->getProjectId());
-		// determine number of days that have elapsed
-		$daysElapsed = $this->getProjectSetting('days-elapsed');
-		if (!isset($daysElapsed)) {
-			$daysElapsed = 0;
-			$this->setProjectSetting('days-elapsed', 0);
-		}
-		$daysElapsed = intval($daysElapsed);
-
-		// determine which sequences to send emails for
-		$urls = [];
-		$settings = $this->getProjectSettings();
-		// echo("<pre>");
-		// print_r($settings);
-		// echo("</pre>");
-		foreach ($settings['sequence']['value'] as $i => $sequence) {
-			$period_every = $settings['periodicity-every']['value'][$i];
-			$period_end = $settings['periodicity-end']['value'][$i];
-			if (isset($period_every) and isset($period_end)) {
-				$period_every = intval($period_every);
-				$period_end = intval($period_end);
-				if (($daysElapsed % $period_every) == 0 and $daysElapsed <= $period_end and $daysElapsed != 0) {
-					$urls[] = $this->getUrl("interview.php") . "&NOAUTH&sequence=$sequence";
-				}
+		$originalPid = $_GET['pid'];
+		foreach($this->framework->getProjectsWithModuleEnabled() as $localProjectId){
+			$_GET['pid'] = $localProjectId;
+			
+			\REDCap::logEvent("Running Email Scheduler", time(), NULL, NULL, NULL, $this->getProjectId());
+			// determine number of days that have elapsed
+			$daysElapsed = $this->getProjectSetting('days-elapsed');
+			if (!isset($daysElapsed)) {
+				$daysElapsed = 0;
+				$this->setProjectSetting('days-elapsed', 0);
 			}
-		}
+			$daysElapsed = intval($daysElapsed);
 
-		$emailSender = $settings['email-sender']['value'];
-		$emailSubject = $settings['email-subject']['value'];
-		$emailBody = $settings['email-body']['value'];
-
-		if (empty($urls) or !isset($emailSender) or !isset($emailSubject) or !isset($emailBody)) {
-			// increment daysElapsed
-			$this->setProjectSetting('days-elapsed', $daysElapsed + 1);
-			exit();
-		}
-
-		// prepare email body by replacing [interview-links] and [interview-urls]
-		$emailBody = str_replace("[interview-urls]", implode($urls, "\r\n"), $emailBody);
-		foreach($urls as $i => $url) {
-			$urls[$i] = "<a href=\"$url\">CAT-MH Interview Link</a>";
-		}
-		$emailBody = str_replace("[interview-links]", implode($urls, "\r\n"), $emailBody);
-
-		// we have links to send so for each participant with a listed email, invite to take interview(s)
-		$data = \REDCap::getData($this->getProjectId(), 'array');
-		foreach($data as $rid => $record) {
-			$eid = array_keys($record)[0];
-			$addressTo = $record[$eid]['participant_email'];
-			if (isset($addressTo)) {
-				foreach($urls as $url) {
-					$success = \REDCap::email($addressTo, $emailSender, $emailSubject, $emailBody);
-					if ($success === false) {
-						\REDCap::logEvent("Failed Sending Interview Email", "$addressTo, $emailSender, $emailSubject, $emailBody", NULL, $rid, $eid, $this->getProjectId());
-					} else {
-						\REDCap::logEvent("Sent Interview Email", "$addressTo, $emailSender, $emailSubject, $emailBody", NULL, $rid, $eid, $this->getProjectId());
+			// determine which sequences to send emails for
+			$urls = [];
+			$settings = $this->getProjectSettings();
+			// echo("<pre>");
+			// print_r($settings);
+			// echo("</pre>");
+			foreach ($settings['sequence']['value'] as $i => $sequence) {
+				$period_every = $settings['periodicity-every']['value'][$i];
+				$period_end = $settings['periodicity-end']['value'][$i];
+				if (isset($period_every) and isset($period_end)) {
+					$period_every = intval($period_every);
+					$period_end = intval($period_end);
+					if (($daysElapsed % $period_every) == 0 and $daysElapsed <= $period_end and $daysElapsed != 0) {
+						$urls[] = $this->getUrl("interview.php") . "&NOAUTH&sequence=$sequence";
 					}
 				}
 			}
-		}
 
-		// increment daysElapsed
-		$this->setProjectSetting('days-elapsed', $daysElapsed + 1);
-		return $urls;
+			$emailSender = $settings['email-sender']['value'];
+			$emailSubject = $settings['email-subject']['value'];
+			$emailBody = $settings['email-body']['value'];
+
+			if (empty($urls) or !isset($emailSender) or !isset($emailSubject) or !isset($emailBody)) {
+				// increment daysElapsed
+				$this->setProjectSetting('days-elapsed', $daysElapsed + 1);
+				exit();
+			}
+
+			// prepare email body by replacing [interview-links] and [interview-urls]
+			$emailBody = str_replace("[interview-urls]", implode($urls, "\r\n"), $emailBody);
+			foreach($urls as $i => $url) {
+				$urls[$i] = "<a href=\"$url\">CAT-MH Interview Link</a>";
+			}
+			$emailBody = str_replace("[interview-links]", implode($urls, "\r\n"), $emailBody);
+
+			// we have links to send so for each participant with a listed email, invite to take interview(s)
+			$data = \REDCap::getData($this->getProjectId(), 'array');
+			foreach($data as $rid => $record) {
+				$eid = array_keys($record)[0];
+				$addressTo = $record[$eid]['participant_email'];
+				if (isset($addressTo)) {
+					foreach($urls as $url) {
+						$success = \REDCap::email($addressTo, $emailSender, $emailSubject, $emailBody);
+						if ($success === false) {
+							\REDCap::logEvent("Failed Sending Interview Email", "$addressTo, $emailSender, $emailSubject, $emailBody", NULL, $rid, $eid, $this->getProjectId());
+						} else {
+							\REDCap::logEvent("Sent Interview Email", "$addressTo, $emailSender, $emailSubject, $emailBody", NULL, $rid, $eid, $this->getProjectId());
+						}
+					}
+				}
+			}
+
+			// increment daysElapsed
+			$this->setProjectSetting('days-elapsed', $daysElapsed + 1);
+		}
+		
+		
+		return "The CAT-MH external module cron method 'cronEmail' ran successfully";
 	}
 	
 	//utility
