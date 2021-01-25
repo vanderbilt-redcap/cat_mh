9bd24aa5801c0a69815ee8d52c08f5679c51c704
Carl Reed <carlreedw@gmail.com>
2021-01-25 12:44:13 -0600

    updates investigate.php

--- investigate.php
+++ investigate.php
@@ -15,13 +15,31 @@ foreach($sequences as $seq) {
 echo "\n\n";
 echo "Reminder settings: " . print_r($reminder_settings, true) . "\n\n";
 
-$records_of_interest = ['1', '58', '59', '60'];
-$records_of_interest = ['1'];
+$record_ids = ['1', '58', '59', '60'];
+$enrollment_field_name = $module->getProjectSetting('enrollment_field');
+$catmh_email_field_name = $module->getProjectSetting('participant_email_field');
+$param_fields = [
+	$module->getRecordIdField(),
+	"$enrollment_field_name",
+	'subjectid',
+	$catmh_email_field_name
+];
+// add filter_fields to getData request
+if (!empty($filter_fields = $module->getProjectSetting('filter-fields')))
+	$param_fields = array_merge($param_fields, $filter_fields);
+$params = [
+	'project_id' => $module->getProjectId(),
+	'return_format' => 'json',
+	'fields' => $param_fields,
+	'records' => $record_ids
+];
+$data = json_decode(\REDCap::getData($params));
+
 $in_30_days = strtotime("+30 days", time());
 $in_30_days_date = date('Y-m-d H:i', $in_30_days);
 echo "30 days from now: $in_30_days_date\n";
 echo "in_30_days (timestamp): $in_30_days\n";
-foreach ($records_of_interest as $record) {
+foreach ($data as $record) {
 	$invites = $module->getInvitationsDue($record, $in_30_days);
 	echo "Invitations due for record $record: \n" . print_r($invites, true) . "\n";
 };
